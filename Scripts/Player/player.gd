6d054be51e378e1d61186166393f159a68db7b69
Andrei <aparsegov@gmail.com>
2024-10-18 00:28:43 +0500

    add basic attack

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -4,13 +4,16 @@ class_name Player
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var health_component: HealthComponent = $HealthComponent
-@onready var hit_box: HitBox = $HitBox
-@onready var collision: CollisionShape2D = $HitBox/CollisionShape2D
+@onready var hit_box: HitBox = $WeaponSlot/HitBox
+
+@onready var collision: CollisionShape2D = $WeaponSlot/HitBox/CollisionShape2D
+@onready var weapon_slot: Node2D = $WeaponSlot
 
 @export var speed : float = 200
 @export var jump_height : float = -300
 @export var damage: float = 10
 
+@export var attacking : bool = false
 var max_health = 20
 var current_health
 
@@ -28,21 +31,29 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_axis("left","right")
-	if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact") and not attacking:
 		interact()
-	if Input.is_action_just_pressed("left"):
+	if Input.is_action_just_pressed("left") and not attacking:
 		sprite.scale.x = abs(sprite.scale.x) * -1
-		hit_box.position.x = abs(hit_box.position.x) * -1
-	if Input.is_action_just_pressed("right"):
+		weapon_slot.scale.x = abs(weapon_slot.scale.x) * -1
+		
+	if Input.is_action_just_pressed("right") and not attacking:
 		sprite.scale.x = abs(sprite.scale.x) * 1
-		hit_box.position.x = abs(hit_box.position.x) * 1
+		weapon_slot.scale.x = abs(weapon_slot.scale.x) * 1
 	if Input.is_action_just_pressed("down") and is_on_floor():
 		position.y += 5
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	if is_on_floor() and Input.is_action_just_pressed("jump") and not attacking:
 		velocity.y = jump_height
+		
+	if attacking == false:
+		collision.call_deferred("set","disabled",true)
+		
+	if velocity.x == 0 and Input.is_action_just_pressed("attack"):
+		attacking = true
+		collision.call_deferred("set","disabled",false)
 		
 	if direction: 
 		velocity.x = speed * direction
@@ -53,10 +64,12 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func update_animation()->void:
-	if velocity.x != 0:
+	if velocity.x != 0 and not attacking:
 		animation.play("run")
-	else:
+	if velocity.x == 0 and not attacking:
 		animation.play("idle")
+	if velocity.x == 0 and attacking:
+		animation.play("attack1")
 	if velocity.y < 0:
 		animation.play("jump")
 	if velocity.y > 0:
